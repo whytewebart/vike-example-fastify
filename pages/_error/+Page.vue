e2b52d9567b245ca597660bbd73cfca9e674735e
Whyte50 <erro50.r5@gmail.com>
2025-05-16 11:19:56 +0100

    feat: enhance error page with dynamic SVGs and brandmark images for improved user experience

--- pages/_error/+Page.vue
+++ pages/_error/+Page.vue
@@ -1,27 +1,79 @@
 <template>
+  <div class="w-full sm:hidden" flex="~ justify-center">
+    <a href="/" class="[all:unset]">
+      <img
+        src="/brandmark.png"
+        width="100px"
+        alt=""
+        v-gsap:error-application-brandmark
+      />
+    </a>
+  </div>
+
   <div class="mt-8" grid="~ justify-items-center gap-y-4">
-    <Button severity="secondary" size="small" class="bg-stone-200/50! capitalize hover:cursor-default">{{ abortReason }}</Button>
-    <img src="./404.svg" alt="" class="w-full">
+    <Button
+      severity="secondary"
+      size="small"
+      class="bg-stone-200/50! capitalize hover:cursor-default"
+      >{{ abortReason }}</Button
+    >
+    <img :src="is404 ? notFound : wentWrong" alt="" class="w-full" />
     <p class="text-center text-gray-5 max-w-sm">
-      The page you are looking for does not exist or an error occurred. Try the links below.
+      The page you are looking for does not exist or an error occurred. Try the
+      links below.
     </p>
 
     <div grid="~ cols-3 gap-x-2">
-      <Button severity="secondary" size="small" class="capitalize" v-tooltip="'Click to clear up authentication errors'">Home</Button>
-      <Button severity="secondary" size="small" class="capitalize" v-tooltip.bottom="'Go to Dashboard'" as="a" href="mailto:wills.yte50@gmail.com">Let's Work Together</Button>
-      <Button severity="secondary" size="small" class="capitalize" v-tooltip.left="'See your organizations'" as="a" href="/work">Portfolio '24</Button>
+      <Button severity="secondary" size="small" class="capitalize">Home</Button>
+      <Button
+        severity="secondary"
+        size="small"
+        class="capitalize"
+        as="a"
+        href="mailto:wills.yte50@gmail.com"
+        >Let's Work Together</Button
+      >
+      <Button
+        severity="secondary"
+        size="small"
+        class="capitalize"
+        as="a"
+        href="/work"
+        >Portfolio '24</Button
+      >
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const pageContext = usePageContext()
-let { is404, abortReason } = pageContext.value
+import notFound from "./404.svg";
+import wentWrong from "./500.svg";
+
+const pageContext = usePageContext();
+let { is404, abortReason } = pageContext.value;
 if (!abortReason) {
-  abortReason = is404 ? 'Page not found.' : 'Something went wrong.'
+  abortReason = is404 ? "Page not found." : "Something went wrong.";
 }
+
+useGsapTimeline(["error-application-brandmark"], {}, ({ gsap, elements }) => {
+  const key = elements["error-application-brandmark"];
+  
+  document.querySelector(key)?.addEventListener("mouseenter", (e) => {
+    gsap.to(key, {
+      // scale: 1.5,
+      rotate: 360,
+      duration: 0.3,
+    });
+  });
+
+  document.querySelector(key)?.addEventListener("mouseleave", (e) => {
+    gsap.to(key, {
+      // scale: 1,
+      duration: 0.3,
+      rotate: 0,
+    });
+  });
+});
 </script>
 
-<style>
-
-</style>
+<style></style>
