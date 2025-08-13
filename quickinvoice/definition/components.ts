b692cd207b4df7a545eb061f8af8079b15f62d97
Whyte50 <erro50.r5@gmail.com>
2025-08-13 11:57:05 +0100

    refactor: remove 'condensed' option from fixedTable component styles

--- quickinvoice/definition/components.ts
+++ quickinvoice/definition/components.ts
@@ -522,7 +522,6 @@ const fixedTable: ComponentDefinition = {
                 'dark',
                 'newspaper',
                 'striped',
-                'condensed'
             ],
         },
         {
