994aeda8f27a03ded57d8453eb745dde5f3b3b57
Whyte50 <erro50.r5@gmail.com>
2025-09-08 06:07:16 +0100

    fix: configure Vercel plugin with additional endpoints for SSR

--- vite.config.ts
+++ vite.config.ts
@@ -27,8 +27,23 @@ const config: UserConfig = {
       dirs: ["./components"],
       directoryAsNamespace: true,
     }),
-    vercel()
+    vercel({
+      source: "/.*",
+    })
   ],
+
+  vercel: {
+    additionalEndpoints: [
+      {
+        // entry file to the server. Default export must be a node server or a function
+        source: "server/vike.ts",
+        // replaces default Vike target
+        destination: "ssr_",
+        // already added by default Vike route
+        route: false,
+      },
+    ],
+  },
 
   resolve: {
     alias: {
