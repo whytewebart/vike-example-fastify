719ef96c0fa80771e5f43b89cf131b2efd7dfda3
whytewebart <wills.yte50@gmail.com>
2026-07-07 17:15:44 +0100

    testing withiut db

--- server/plugins/database/index.ts
+++ server/plugins/database/index.ts
@@ -1,34 +1,33 @@
-import fastifyPlugin from 'fastify-plugin';
-import { drizzle } from 'drizzle-orm/libsql';
-import seed from './tests/seed.ts';
+import fastifyPlugin from "fastify-plugin";
+// import { drizzle } from 'drizzle-orm/libsql';
+// import seed from './tests/seed.ts';
 
 export default fastifyPlugin(
-    async (fastify, opts): Promise<void> => {
-        const db = drizzle({
-            connection: {
-                url: fastify.config.DB_FILE_NAME
-            }
-        });
+	async (fastify, opts): Promise<void> => {
+		// const db = drizzle({
+		//     connection: {
+		//         url: fastify.config.DB_FILE_NAME
+		//     }
+		// });
 
-        fastify.decorate('db', db);
+		// fastify.decorate('db', db);
 
-        if (fastify.config.DRIZZLE_SEED)
-            await seed.execute(fastify, opts);
+		// if (fastify.config.DRIZZLE_SEED)
+		//     await seed.execute(fastify, opts);
 
-        fastify.ready(err => {
-            if (err) throw new Error(err.message);
-            fastify.log.info(`DB initialized`)
-        })
-    },
-    {
-        name: 'database', dependencies: [
-            "validators", "fastify-env"
-        ]
-    }
-)
+		fastify.ready((err) => {
+			if (err) throw new Error(err.message);
+			fastify.log.info(`DB initialized`);
+		});
+	},
+	{
+		name: "database",
+		dependencies: ["validators", "fastify-env"],
+	},
+);
 
-declare module 'fastify' {
-    export interface FastifyInstance {
-        db: ReturnType<typeof drizzle>;
-    }
+declare module "fastify" {
+	export interface FastifyInstance {
+		db: ReturnType<typeof drizzle>;
+	}
 }
